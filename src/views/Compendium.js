ee1ba518ccfa3b2991e29cda805f29d761cd34e8
Denzel <dbartolaba123@gmail.com>
2022-04-28 10:29:53 -0700

    set up dropdown and handle change event

--- src/views/Compendium.js
+++ src/views/Compendium.js
@@ -5,6 +5,15 @@ export default function Compendium() {
   const [characters, setCharacters] = useState([]);
   const [loading, setLoading] = useState(true);
   const [status, setStatus] = useState('Alive');
+  const [results, setResults] = useState([]);
+
+  const handleChange = (event) => {
+    setStatus(event.target.value);
+
+    const characterStatus = characters.filter((character) =>
+      character.status);
+    setResults(characterStatus);
+  }
 
   useEffect(() => {
     const getCharacters = async () => {
@@ -15,6 +24,15 @@ export default function Compendium() {
     getCharacters();
   }, [])
   return (
-    <div>Compendium</div>
+    <div>
+      <label>
+        Current Character Status:
+        <select value={status} onChange={handleChange}>
+          <option value='Alive'>Alive</option>
+          <option value='Dead'>Dead</option>
+          <option value='Unknown'>Unknown</option>
+        </select>
+      </label>
+    </div>
   )
 }
