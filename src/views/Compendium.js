85652d3688884af6ed78b95a10434f330296c2ca
Denzel <dbartolaba123@gmail.com>
2022-04-28 11:15:23 -0700

    got my characters rendering and now shows status

--- src/views/Compendium.js
+++ src/views/Compendium.js
@@ -4,35 +4,59 @@ import { rickAndMortyFetch } from "../services/fetch-utils"
 export default function Compendium() {
   const [characters, setCharacters] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [status, setStatus] = useState('Alive');
+  const [status, setStatus] = useState('Dead');
   const [results, setResults] = useState([]);
+
+  const currentStatus = !!status.length;
+  const noResults = currentStatus && !results.length;
+  const characterList = currentStatus ? results : characters;
 
   const handleChange = (event) => {
     setStatus(event.target.value);
-
-    const characterStatus = characters.filter((character) =>
-      character.status);
-    setResults(characterStatus);
   }
 
   useEffect(() => {
     const getCharacters = async () => {
       const characterList = await rickAndMortyFetch();
       setCharacters(characterList);
+      setStatus('Alive');
       setLoading(false);
     }
     getCharacters();
   }, [])
+
+  useEffect(() => {
+    const characterStatus = characters.filter((character) => {
+      return character.status === status;
+    });
+    setResults(characterStatus);
+  }, [status])
   return (
     <div>
-      <label>
-        Current Character Status:
-        <select value={status} onChange={handleChange}>
-          <option value='Alive'>Alive</option>
-          <option value='Dead'>Dead</option>
-          <option value='Unknown'>Unknown</option>
-        </select>
-      </label>
+      <h2>Rick and Morty Characters!!</h2>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <><label>
+          Current Character Status:
+          <select value={status} onChange={handleChange}>
+            <option value='Alive'>Alive</option>
+            <option value='Dead'>Dead</option>
+            <option value='unknown'>Unknown</option>
+          </select>
+        </label><ul>
+            {characterList.map((character) => {
+              return (
+                <li>
+                  <img src={character.img} />
+                  <h3>{character.name}</h3>
+                  <p>{character.status}</p>
+                </li>
+              );
+            })}
+          </ul></>
+      )}
+      {noResults && <p>No Results Found ):</p>}
     </div>
   )
 }
