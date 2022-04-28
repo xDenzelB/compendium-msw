6b9aee30f48ce9e07cfe08ee2faff9f57f4a432f
Denzel <dbartolaba123@gmail.com>
2022-04-28 10:14:25 -0700

    set up useEffect for characters

--- src/views/Compendium.js
+++ src/views/Compendium.js
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react"
 import { rickAndMortyFetch } from "../services/fetch-utils"
 
 export default function Compendium() {
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [status, setStatus] = useState('Alive');
+
+  useEffect(() => {
+    const getCharacters = async () => {
+      const characterList = await rickAndMortyFetch();
+      setCharacters(characterList);
+      setLoading(false);
+    }
+    getCharacters();
+  }, [])
   return (
     <div>Compendium</div>
   )
